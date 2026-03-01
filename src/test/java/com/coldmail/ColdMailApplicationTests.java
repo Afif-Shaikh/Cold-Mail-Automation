dfe609daed5c53dbed737f868d202cc795935c2d
Afif-Shaikh <shaikhafif5@gmail.com>
2026-03-01 14:34:54 +0530

    Fix build configuration

---
+++ src/test/java/com/coldmail/ColdMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coldmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Basic test to verify application context loads
+    }
+}
